aec6d1a9d05928fd91788edcb0e55edcc8c1da8e
Martin Mutinda <eminentwriters8@gmail.com>
2025-09-29 13:05:51 +0300

    Add UserProfile model and Pillow for profile pictures

--- clients/management/commands/create_admin.py
+++ clients/management/commands/create_admin.py
@@ -5,8 +5,8 @@ class Command(BaseCommand):
     help = 'Create superuser'
 
     def handle(self, *args, **options):
-        if not User.objects.filter(username='admin').exists():
-            User.objects.create_superuser('admin', 'admin@example.com', 'admin123')
-            self.stdout.write(self.style.SUCCESS('Superuser created successfully'))
+        if not User.objects.filter(username='mutinda').exists():
+            User.objects.create_superuser('mutinda', 'eminentwriters8@gmail.com', '123admin')
+            self.stdout.write(self.style.SUCCESS('Superuser created: mutinda / 123admin'))
         else:
             self.stdout.write(self.style.WARNING('Superuser already exists'))
