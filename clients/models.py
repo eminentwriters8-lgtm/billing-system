c281a2fd92157e0858d8f2397657a4b968e44939
Martin Mutinda <eminentwriters8@gmail.com>
2025-09-29 13:04:01 +0300

    Add UserProfile model and Pillow for profile pictures

--- clients/models.py
+++ clients/models.py
@@ -99,3 +99,12 @@ class Payment(models.Model):
     
     def __str__(self):
         return f"Payment {self.amount} - {self.client.username}"
+from django.contrib.auth.models import User
+from django.db import models
+
+class UserProfile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    profile_picture = models.ImageField(upload_to='profile_pics/', blank=True, null=True)
+    
+    def __str__(self):
+        return f"{self.user.username} Profile"
